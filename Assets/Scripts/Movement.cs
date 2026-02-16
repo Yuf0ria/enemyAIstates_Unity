39d5e4f394d2e50d91be325b0ade94d3a633cd2e
Dani F. <114934334+Yuf0ria@users.noreply.github.com>
2026-02-16 13:23:15 +0800

    navmeshLink: Not Working

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,50 +1,85 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
-    //vectors for movement
-    private Vector3 Velocity;
-    private Vector3 PlayerMovementInput;
-    private Vector2 PlayerMouseInput;
-    private float xRotation;
-
-    [Header("Components Needed")]
-    [SerializeField] private Transform PlayerCamera;
-    [SerializeField] private CharacterController Controller;
-    [SerializeField] private Transform Player;
+    #region Vector Components
+        private Vector3 _velocity;
+        private Vector3 _playerMovementInput;
+        private Vector2 _playerMouseInput;
+        private float _xRotation;
+    #endregion
+    #region Movement Components
+        [SerializeField] private Transform _playerCamera;
+        [SerializeField] private CharacterController _controller;
+        [SerializeField] private Transform _player;
+        [SerializeField] private NavMeshAgent _agent;
+    #endregion
+    
     //Movement
-    // float Jump = 3;
+    float Jump = 6;
     float Speed = 3;
-    private float Sensetivity = 1;
+    private float Sensitivity = 1;
     private float Gravity = 9.81f;
+    
+    [SerializeField] private float jumpDuration = 0.8f;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked; 
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     void Update()
     {
 
-        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-
+        _playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        _playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         MovePlayer();
         MoveCamera();
+        
     }
+    
+    bool jumping = false;
+    
+    System.Collections.IEnumerator DoJump(OffMeshLinkData data)
+    {
+        jumping = true;
+        Vector3 start = _agent.transform.position;
+        Vector3 end = data.endPos;
+        float elapsed = 0f;
+
+        while (elapsed < jumpDuration)
+        {
+            float t = elapsed / jumpDuration;
+            float height = Mathf.Sin(Mathf.PI * t) * Jump;
+            _agent.transform.position = Vector3.Lerp(start, end, t) + Vector3.up * height;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        _agent.transform.position = end;
+        _agent.CompleteOffMeshLink();
+        jumping = false;
+    }
+    
     private void MovePlayer()
     {
-        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput);
+        Vector3 MoveVector = transform.TransformDirection(_playerMovementInput);
 
-        if (Controller.isGrounded)
+        if (_controller.isGrounded)
         {
-            Velocity.y = -1f;
+            _velocity.y = -1f;
 
-            // if (Input.GetKeyDown(KeyCode.Space))
-            // {
-            //     Velocity.y = Jump;
-            // }
-
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _velocity.y = Jump;
+                if (_agent.isOnOffMeshLink && !jumping)
+                {
+                    StartCoroutine(DoJump(_agent.currentOffMeshLinkData));
+                }
+            }
             if (Input.GetKeyDown(KeyCode.LeftShift))
             {
                 Speed *= 3;
@@ -53,22 +88,21 @@ public class Movement : MonoBehaviour
             {
                 Speed = 3;
             }
-            Controller.Move(MoveVector * Speed * Time.deltaTime);
+            _controller.Move(MoveVector * Speed * Time.deltaTime);
         }
         else
         {
-            Velocity.y += Gravity * -2f * Time.deltaTime;
+            _velocity.y += Gravity * -2f * Time.deltaTime;
         }
-        
-        Controller.Move(Velocity * Time.deltaTime);
+        _controller.Move(_velocity * Time.deltaTime);
 
     }
     private void MoveCamera()
     {
-        xRotation -= PlayerMouseInput.y * Sensetivity;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        _xRotation -= _playerMouseInput.y * Sensitivity;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
 
-        transform.Rotate(0f, PlayerMouseInput.x * Sensetivity, 0f);
-        PlayerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(0f, _playerMouseInput.x * Sensitivity, 0f);
+        _playerCamera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
     }
 }
--- Assets/Scripts/Spline.cs
+++ Assets/Scripts/Spline.cs
@@ -13,23 +13,27 @@ public class Spline : MonoBehaviour
     private Vector3 CalculatePoints(float value01, Vector3 startPos, Vector3 endPos, Vector3 midPos)
     {
         value01 = Mathf.Clamp01(value01);
-        Vector3 startMiddle = Vector3.Lerp(startPos, midPos, value01);
-        Vector3 middleEnd = Vector3.Lerp(midPos, endPos, value01);
-        return Vector3.Lerp(startMiddle, middleEnd, value01);
+        Vector3 startMiddle = Vector3.Lerp(startPos, midPos, value01),
+            middleEnd = Vector3.Lerp(midPos, endPos, value01);
+        return Vector3.Lerp(startMiddle, middleEnd, value01); //using lerp to smooth things out
     }
-    public Vector3 CalculatePosition(float interpolationAmount01)
+    //calculates the interpolation amount of the spline
+    private Vector3 CalculatePosition(float interpolationAmount01)
         => CalculatePoints(interpolationAmount01, _start.position, _end.position, _middle.position);
     
     //calculates the fluent movement when jumping, referenced sunny valley studio here
     #region CustomCalculations
-        public Vector3 CalculatePositonStart(float interpolationAmount01, Vector3 startPos) => CalculatePoints(interpolationAmount01, startPos, _end.position, _middle.position);
-        
-        public Vector3 CalculatePositionEnd(float interpolationAmount01, Vector3 endPos) => CalculatePoints(interpolationAmount01, _start.position, endPos,  _middle.position);
+        public Vector3 CalculatePositonStart(float interpolationAmount01, Vector3 startPos) => 
+            CalculatePoints(interpolationAmount01, startPos, _end.position, _middle.position);
+        public Vector3 CalculatePositionEnd(float interpolationAmount01, Vector3 endPos) => 
+            CalculatePoints(interpolationAmount01, _start.position, endPos,  _middle.position);
     #endregion
 
     public void SetPoints(Vector3 startPos, Vector3 midPos, Vector3 endPos)
     {
-        if (_start != null && _middle != null && _end != null)
+        if (_start != null && 
+            _middle != null && 
+            _end != null)
         {
             _start.position = startPos;
             _middle.position = midPos;
@@ -57,7 +61,6 @@ public class Spline : MonoBehaviour
                     ? _end.position : 
                     CalculatePosition((i + 1) / (float)granularity);  
                 Gizmos.DrawLine(startPoint, endPoint);
-
             }
         }
             
