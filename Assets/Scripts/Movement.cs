937f3455fc9a1ceaea446f7023f5cdfa532b201e
Dani F. <114934334+Yuf0ria@users.noreply.github.com>
2026-02-21 11:14:21 +0800

    this is the updated repository

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
@@ -10,58 +8,35 @@ public class Movement : MonoBehaviour
         private Vector2 _playerMouseInput;
         private float _xRotation;
     #endregion
+    
     #region Movement Components
+        [Header("Movement Components")]
         [SerializeField] private Transform _playerCamera;
         [SerializeField] private CharacterController _controller;
-        [SerializeField] private Transform _player;
-        [SerializeField] private NavMeshAgent _agent;
     #endregion
     
-    //Movement
-    float Jump = 6;
-    float Speed = 3;
-    private float Sensitivity = 1;
-    private float Gravity = 9.81f;
+    #region Movement Settings
+        private const float Jump = 8f;  // Increased from 6 to jump higher than enemies
+        private const float Speed = 3f;
+        private const float Sensitivity = 1f;
+        private const float Gravity = 9.81f;
+    #endregion
     
-    [SerializeField] private float jumpDuration = 0.8f;
-
+    private float currentSpeed;
+    
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; 
-        _agent = GetComponent<NavMeshAgent>();
+        Cursor.lockState = CursorLockMode.Locked;
+        currentSpeed = Speed;
     }
 
     void Update()
     {
-
         _playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         _playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        MovePlayer();
-        MoveCamera();
         
-    }
-    
-    bool jumping = false;
-    
-    System.Collections.IEnumerator DoJump(OffMeshLinkData data)
-    {
-        jumping = true;
-        Vector3 start = _agent.transform.position;
-        Vector3 end = data.endPos;
-        float elapsed = 0f;
-
-        while (elapsed < jumpDuration)
-        {
-            float t = elapsed / jumpDuration;
-            float height = Mathf.Sin(Mathf.PI * t) * Jump;
-            _agent.transform.position = Vector3.Lerp(start, end, t) + Vector3.up * height;
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        _agent.transform.position = end;
-        _agent.CompleteOffMeshLink();
-        jumping = false;
+        MoveCamera();
+        MovePlayer();
     }
     
     private void MovePlayer()
@@ -71,32 +46,28 @@ public class Movement : MonoBehaviour
         if (_controller.isGrounded)
         {
             _velocity.y = -1f;
-
+            
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _velocity.y = Jump;
-                if (_agent.isOnOffMeshLink && !jumping)
-                {
-                    StartCoroutine(DoJump(_agent.currentOffMeshLinkData));
-                }
             }
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                Speed *= 3;
-            }
-            else if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                Speed = 3;
-            }
-            _controller.Move(MoveVector * Speed * Time.deltaTime);
+            
+            if (Input.GetKey(KeyCode.LeftShift))
+                currentSpeed = Speed * 3f;
+            else
+                currentSpeed = Speed;
         }
         else
         {
             _velocity.y += Gravity * -2f * Time.deltaTime;
         }
+        
+        // Apply horizontal movement
+        _controller.Move(MoveVector * currentSpeed * Time.deltaTime);
+        // Apply vertical movement (jump/gravity)
         _controller.Move(_velocity * Time.deltaTime);
-
     }
+    
     private void MoveCamera()
     {
         _xRotation -= _playerMouseInput.y * Sensitivity;
